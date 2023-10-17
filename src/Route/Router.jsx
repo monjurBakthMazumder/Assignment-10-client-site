1e5953c5ab32fe5de2e0c266acad49b0d60a1d1c
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-17 22:18:52 +0600

    add route

--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -2,6 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../Layout/MainLayout';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 import Home from '../Pages/Home/Home';
+import AddProduct from '../Pages/AddProduct/AddProduct';
+import Cart from '../Pages/Cart/Cart';
+import Update from '../Pages/Update/Update';
+import Details from '../Pages/Details/Details';
+import Login from '../Pages/Authentication/Login/Login';
+import Register from '../Pages/Authentication/Register/Register';
 
 const Router = createBrowserRouter([
     {
@@ -12,6 +18,30 @@ const Router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
+            },
+            {
+                path: '/add-products',
+                element: <AddProduct/>
+            },
+            {
+                path: '/cart',
+                element: <Cart/>
+            },
+            {
+                path: '/update/:id',
+                element: <Update/>
+            },
+            {
+                path: '/Product/:id',
+                element: <Details/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/register',
+                element: <Register/>
             }
         ]
     }
