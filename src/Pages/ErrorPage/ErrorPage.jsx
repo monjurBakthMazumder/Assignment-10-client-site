38e6bc6ee17fda46e6dd12b096abacc29ac9f7eb
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-18 01:42:57 +0600

    create error page ui

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>
-            ErrorPage
+        <div className="flex flex-col justify-center items-center gap-5 h-screen w-full">
+            <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold">404</h1>
+            <p className="text-2xl md:text-3xl lg:text-4xl font-semibold">Page not found</p>
+            <Link to={'/'} className="btn btn-primary">Go Home</Link>
         </div>
     );
 };
