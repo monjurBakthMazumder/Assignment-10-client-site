93d7f33f2848d3aff43aac5ff8df692507a0499e
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-10-19 05:05:40 +0600

    update brand card

--- src/Component/BrandCard/BrandCard.jsx
+++ src/Component/BrandCard/BrandCard.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const BrandCard = ({brandData}) => {
     const {brand, img} = brandData || {}
     return (
-        <div className="card border">
+        <Link to={`/brand/${brand}`} className="card border">
             <figure className='h-40 mt-5'><img src={img} alt={`image og ${brand}`} className='h-full'/></figure>
             <div className="card-body">
-                <Link to={`/brand/${brand}`} className="card-title mx-auto font-bold">{brand}</Link>
+                <button className="card-title mx-auto font-bold">{brand}</button>
             </div>
-        </div>
+        </Link>
     );
 };
 
